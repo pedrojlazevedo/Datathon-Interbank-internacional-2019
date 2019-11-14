3d215b9047981efeb8691015d670688fac4a8d78
Bernardo Ramos <up201505092@fe.up.pt>
2019-11-14 02:32:56 +0000

    merging is not working for rcc because of multi index, printed in seperte files but still have to correct header names

--- rcc_treatment.py
+++ rcc_treatment.py
@@ -13,31 +13,27 @@ def diff(first, second):
 #
 # Working with Campanhas table
 
-with open('rcc_treatment.csv', mode='w', newline='') as csv_file:
-    csv_f = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    rcc = pd.read_csv(r"C:\Users\USER\Desktop\datathon-pedro\Datathon\interbank-internacional-2019\ib_base_rcc\ib_base_rcc.csv")
-    ## unique keys
-    ids         = rcc.id_persona.unique()
-    products = rcc.producto.unique()
-    test = rcc
-    newDF = pd.DataFrame()
-    aux=0
-    d = {'mto_saldo': ['mean','sum'], 'clasif': ['mean','min','max'], 'rango_mora': ['mean','min','max']} 
-    for product in products:
-        product_name_aux=str(product)
-        product_name=product_name_aux.replace(" ", "")+str('Count')
-        ## adding new metrics
-        totalCount = test.groupby(["id_persona","codmes"])[['producto','mto_saldo','rango_mora','clasif']] \
-        .apply(lambda x: x[x['producto']==product]) \
-        .reset_index()
-        finalCount = totalCount.groupby(["id_persona","codmes"]).agg(d)
-        print(finalCount)
-        if aux==0:
-            newDF=totalCount
-        else:
-            ## merging metrics -> find out to do a full outer join (not losing codmes when relevant)
-            newDF = pd.merge(newDF, finalCount, on=['id_persona','codmes'], how='outer')
-        aux=aux+1
-    path = r'C:\Users\USER\Desktop\datathon-pedro\Datathon\interbank-internacional-2019\data_generation'
-    camp_file = str('countCampaigns.csv')
-    newDF.to_csv(os.path.join(path,camp_file),index=False)
+
+
+rcc = pd.read_csv(r"C:\Users\USER\Desktop\datathon-pedro\Datathon\interbank-internacional-2019\ib_base_rcc\ib_base_rcc.csv")
+## unique keys
+ids         = rcc.id_persona.unique()
+products = rcc.producto.unique()
+test = rcc.head(200)
+aux=0
+d = {'mto_saldo': ['mean','sum'], 'clasif': ['mean','min','max'], 'rango_mora': ['mean','min','max']} 
+for product in products:
+    product_name_aux=str(product)
+    product_name=product_name_aux.replace(" ", "")
+    ## adding new metrics
+    totalCount = test.groupby(["id_persona","codmes"])[['producto','mto_saldo','rango_mora','clasif']] \
+    .apply(lambda x: x[x['producto']==product]) \
+    .reset_index()
+    finalCount = totalCount.groupby(["id_persona","codmes"]).agg(d)
+    print(finalCount)
+    path = r'C:\Users\USER\Desktop\datathon-pedro\Datathon\interbank-internacional-2019\data_generation\rcc'
+    camp_file = product_name + str('.csv')
+    final_path = path+camp_file
+    with open(final_path, 'w') as f:
+        f.write('\n'.join([','.join(h) for h in zip(*finalCount.columns)]) + '\n')
+        finalCount.to_csv(final_path, mode='a', index=True, header=False)
