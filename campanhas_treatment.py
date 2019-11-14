491b5f05142120c5e1a6dbca59710bee6f75978f
beerzyp <up201505092@fe.up.pt>
2019-11-14 01:59:49 +0000

    Update campanhas_treatment.py

--- campanhas_treatment.py
+++ campanhas_treatment.py
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-import pandas as pd
-import numpy as np
-import csv
-import math
-import re
-import numpy_indexed as npi
-
-#
-# Working with Campanhas table
-with open('campaign_treatment.csv', mode='w', newline='') as csv_file:
-    csv_f = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    base_camp = pd.read_csv("interbank-internacional-2019/ib_base_campanias/ib_base_campanias.csv")
-    #with open("interbank-internacional-2019/ib_base_rcc/ib_base_rcc.csv", parse_dates=["codmes"]) as myfile:
-    base_camp['codmes'] =  pd.to_datetime(base_camp['codmes'], format='%Y%m')
-    ids         = base_camp.id_persona.unique()
-    camp_prod   = base_camp.producto.unique()
-    ids = base_camp.id_persona.unique()
-    '''
-    There are  14.56 % of regists that we want (1)
-    2019-01    54088
-    2019-02    46125
-    2019-03    52387
-    2019-04    60065
-    '''
-    meses = {
-        201901: slice(201808, 201810),
-        201902: slice(201809, 201811),
-        201903: slice(201810, 201812),
-        201904: slice(201811, 201901),
-        201905: slice(201812, 201902),
-        201906: slice(201901, 201903),
-        201907: slice(201902, 201904)
-    }
-
-    campaigns = base_camp.producto.unique()
-    campaigns = np.insert(campaigns,0,'id_persona')
-    campaigns = np.insert(campaigns,1,'codmes')
-    campaigns = np.insert(campaigns,1,'canal_asignado')
-    csv_f.writerow(campaigns)
-    aux=0
-    final = base_camp.groupby(["id_persona","codmes","producto"]).first()
-    
-    print(final)
-    print(aux)
-=======
 import pandas as pd
 import numpy as np
 import csv
@@ -58,28 +12,6 @@ def diff(first, second):
         return [item for item in first if item not in second]
 #
 # Working with Campanhas table
-'''
-meses = {
-    201901: slice(201808, 201810),
-    201902: slice(201809, 201811),
-    201903: slice(201810, 201812),
-    201904: slice(201811, 201901),
-    201905: slice(201812, 201902),
-    201906: slice(201901, 201903),
-    201907: slice(201902, 201904)
-}
-complementos = []
-for mes in meses.keys():
-    print("*"*10, mes, "*"*10)
-    res = pd.concat([
-        camp_canal.loc[meses[mes]].groupby("id_persona").sum(),
-        camp_prod.loc[meses[mes]].groupby("id_persona").sum()
-        
-    ], axis=1)
-    res["codmes"] = mes
-    res = res.reset_index().set_index(["id_persona", "codmes"]).astype("float32")
-    complementos.append(res)
-'''
 with open('campaign_treatment.csv', mode='w', newline='') as csv_file:
     csv_f = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     base_camp = pd.read_csv("interbank-internacional-2019/ib_base_campanias/ib_base_campanias.csv")
@@ -87,12 +19,6 @@ with open('campaign_treatment.csv', mode='w', newline='') as csv_file:
     ids         = base_camp.id_persona.unique()
     campaigns = base_camp.producto.unique()
     ## headers
-    ''' campaigns = np.insert(campaigns,0,'id_persona')
-    campaigns = np.insert(campaigns,1,'codmes')
-    campaigns = np.insert(campaigns,1,'canal_asignado')
-    csv_f.writerow(campaigns)'''
-    ## counts 1 393 833 1391833
-    ##1 658 876‬
     test = base_camp
     newDF = pd.DataFrame()
     aux=0
@@ -113,4 +39,3 @@ with open('campaign_treatment.csv', mode='w', newline='') as csv_file:
     path = r'C:\Users\USER\Desktop\datathon-pedro\Datathon\interbank-internacional-2019\data_generation'
     camp_file = str('countCampaigns.csv')
     newDF.to_csv(os.path.join(path,camp_file),index=False)
->>>>>>> 692bf96c7585b2ccc9bdbc0ad10054dec48d3166
