2a82394beeea126c677bd90179f8610d93431184
Bernardo Ramos <up201505092@fe.up.pt>
2019-11-14 23:32:55 +0000

    digital treatment:min max avg for all columns, also eliminated 6 columns who had no values

---
+++ digital_treatment.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import csv
+import math
+import re
+import numpy_indexed as npi
+import sys
+import os
+
+def diff(first, second):
+        second = set(second)
+        return [item for item in first if item not in second]
+#
+# Working with Campanhas table
+
+
+
+digital = pd.read_csv(r"C:\Users\USER\Desktop\datathon-pedro\Datathon\interbank-internacional-2019\ib_base_digital\ib_base_digital.csv")
+## unique keys
+ids         = digital.id_persona.unique()
+test = digital
+aux=0
+# Data Analysis - Has any values different then 0
+print((test['time_tc']!=0).sum())
+print((test['time_ctasld']!=0).sum())
+print((test['time_mllp']!=0).sum())
+print((test['time_mllst']!=0).sum())
+print((test['busqmc']!=0).sum())
+print((test['busqamex']!=0).sum())
+print(len(test.columns))
+# Dropping columns containing only zeroes
+digAnalyzed = test.drop(['time_tc','time_ctasld','time_mllp','time_mllst','busqmc','busqamex'],axis=1) 
+# Confirming all columns have atleast 1 relevant result
+print(len(digAnalyzed.columns))
+for cols in digAnalyzed.columns:
+    print((digAnalyzed[cols]!=0).sum())
+# Converting codday to codmes and renaming it
+digAnalyzed["codday"] = digAnalyzed["codday"].apply(lambda x : int(x/100))
+finalDigital = digAnalyzed.rename(columns={"codday":"codmes"})
+# Calculating mean min max for every column
+totalCount = finalDigital.groupby(['id_persona','codmes']).aggregate(['mean','min','max'])
+path = r'C:\Users\USER\Desktop\datathon-pedro\Datathon\interbank-internacional-2019\data_generation'
+digital_file = str('digital_new.csv')
+# Converting multi level header to single level
+totalCount.columns = [' '.join(col).strip() for col in totalCount.columns.values]
+totalCount.to_csv(os.path.join(path,digital_file),index=True)
