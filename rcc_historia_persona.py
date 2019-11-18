1c4febfa8b77fc9a90259cdd85e524c413ee915f
pedrojlazevedo <up201306026@fe.up.pt>
2019-11-18 19:15:06 +0000

    Rango Count by importance

--- rcc_historia_persona.py
+++ rcc_historia_persona.py
@@ -6,6 +6,28 @@ import re
 import numpy_indexed as npi
 import sys
 import os
+
+def rango_weak(series):
+    count = 0
+    for index, value in series.items():
+        if value < 2:
+            count += 1
+    return count
+
+def rango_medium(series):
+    count = 0
+    for index, value in series.items():
+        if value >= 2 and value < 4:
+            count += 1
+    return count
+
+def rango_strong(series):
+    count = 0
+    for index, value in series.items():
+        if value > 4:
+            count += 1
+    return count
+
 # Working with Campanhas table
 
 rcc = pd.read_csv("interbank-internacional-2019\ib_base_rcc\ib_base_rcc.csv")
@@ -19,12 +41,19 @@ print((rcc["clasif"].isna()).sum())
 # Filling missing values of rcc[clasif] and rcc[rango_mora] to -1
 rcc.clasif.fillna(value=-1,inplace=True)
 rcc.rango_mora.fillna(value=-1,inplace=True)
+
+# TODO:Trying to create new arrays with count of rango_mora! 
+rcc = rcc.sort_values(by=['rango_mora', 'id_persona'], ascending=False)
+rcc = rcc.head(100)
+
 # Aggregating by metrics for each [[id_persona,codmes],[bank]] -> for every product
-d = {'mto_saldo': ['mean','sum'], 'clasif': ['mean','min','max'], 'rango_mora': ['mean','min','max']} 
-rcc_banco = rcc.groupby(["codmes", "id_persona","cod_banco", "producto"]).agg(d) \
-    .unstack(level=3, fill_value=0).reset_index().set_index("codmes").sort_index().astype("int32")
+d = {"cod_banco": ['count'],'mto_saldo': ['mean','sum'], 'clasif': ['mean','min','max'], \
+    'rango_mora': ['mean','min','max', rango_weak, rango_medium, rango_strong]} 
+rcc_banco = rcc.groupby(["codmes", "id_persona", "producto"]).agg(d) \
+    .unstack(level=2, fill_value=0).reset_index().set_index("codmes").sort_index().astype("int32")
+
 rcc_banco.columns = [' '.join(col).strip() for col in rcc_banco.columns.values]
 
-path = r'C:\Users\USER\Desktop\datathon-final\Datathon\interbank-internacional-2019\data_generation'
+path = r'interbank-internacional-2019\data_generation'
 rcc_file = str('rcc_historia_persona.csv')
 rcc_banco.to_csv(os.path.join(path,rcc_file))
--- rcc_transform.py
+++ rcc_transform.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import csv
 
-campanias = pd.read_csv(r"interbank-internacional-2019\data_generation\rcc_new.csv")
+rcc = pd.read_csv(r"interbank-internacional-2019\data_generation\rcc_new.csv")
 
 meses = {
     201901: slice(201808, 201810),
@@ -13,44 +13,69 @@ meses = {
     201907: slice(201902, 201904)
 }
 
-complementos = []
-ids          = campanias.id_persona.unique()
-cols = campanias.columns
+complementos    = []
+ids             = rcc.id_persona.unique()
+cols            = rcc.columns
 aux = 1
 print(len(ids))
-with open('rcc_final.csv', mode='w', newline='') as csv_file:
+with open(r'interbank-internacional-2019\data_generation\rcc_final.csv', mode='w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     csv_writer.writerow(cols)
+    calcs = ["max", "min", "avg", "count", "sum"]
     for id in ids:
         for mes in meses:
             res = {}
-            res["id_persona"] = id        
-            lines = campanias.loc[campanias['id_persona'] == id]
-            res["codmes"] = mes
+            res["id_persona"] = id
+            res["codmes"] = mes    
+            lines = rcc.loc[rcc['id_persona'] == id]            
             flag = False
 
+            #init variables
             for col in cols:
                 if col == "id_persona" or col == "codmes":
                     continue
-                res[col] = 0
+                elif col == "mto_saldo":
+                    for calc in calcs:
+                        res[col + "_" + calc] = 0
+                elif col == "rango_mora":
+                    res["rango_mora_atraso"] = 0
+                    res["rango_mora_super_atraso"] = 0
+                elif col == "cod_banco"
+                    res["banco_count"] = 0                
+                elif col == "clasif"
+                    res["clasif_avg"] = 0
 
             for index, row in lines.iterrows():
                 month = row["codmes"]
-                if ((mes == 201901 and (month == 201808 or month == 201809 or month == 201810)) or
-                (mes == 201902 and (month == 201809 or month == 201810 or month == 201811)) or
-                (mes == 201903 and (month == 201810 or month == 201811 or month == 201812)) or
-                (mes == 201904 and (month == 201811 or month == 201812 or month == 201901)) or
-                (mes == 201905 and (month == 201812 or month == 201901 or month == 201902)) or
-                (mes == 201906 and (month == 201901 or month == 201902 or month == 201903)) or
-                (mes == 201907 and (month == 201902 or month == 201903 or month == 201904))                
-                ):
+                if ((mes == 201901 and (month == 201807 or month == 201808 or month == 201809)) or
+                    (mes == 201902 and (month == 201808 or month == 201809 or month == 201810)) or
+                    (mes == 201903 and (month == 201809 or month == 201810 or month == 201811)) or
+                    (mes == 201904 and (month == 201810 or month == 201811 or month == 201812)) or
+                    (mes == 201905 and (month == 201811 or month == 201812 or month == 201901)) or
+                    (mes == 201906 and (month == 201812 or month == 201901 or month == 201902)) or
+                    (mes == 201907 and (month == 201901 or month == 201902 or month == 201903))                
+                    ):
                     flag = True
+                    my_list = []
                     for col in cols:
                         if col == "id_persona" or col == "codmes":
                             continue
-                        res[col] += row[col]
+                        elif col == "mto_saldo":
+                            if  res[col + "_" + "max"] < row["mto_saldo"]:
+                                res[col + "_" + "max"] = row["mto_saldo"]
+                            if  res[col + "_" + "min"] > row["mto_saldo"] or res[col + "_" + "min"] == 0:
+                                res[col + "_" + "min"] = row["mto_saldo"]
+                            if  res[col + "_" + "count"]
+                        elif col == "rango_mora":
+                            res["rango_mora_atraso"] = 0
+                            res["rango_mora_super_atraso"] = 0
+                        elif col == "cod_banco"
+                            res["banco_count"] = 0                        
+                        elif col == "clasif"
+                            res["clasif_avg"] = 0
             #print(res)
             if flag:
+                #calcular
                 csv_writer.writerow(res.values())
         if aux%1000 == 0:
             print("Mil Done" + str(aux))
