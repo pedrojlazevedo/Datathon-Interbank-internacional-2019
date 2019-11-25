194af71dd05b1262034823d86ac4023bef2e985a
pedrojlazevedo <up201306026@fe.up.pt>
2019-11-25 11:04:17 +0000

    Fill and Feature selection Adding Vehiculars again

--- notebook.py
+++ notebook.py
@@ -253,6 +253,19 @@ X_train.drop(all_to_remove_new, axis = 1, inplace = True)
 X_test.drop(all_to_remove_new, axis = 1, inplace = True)
 
 
+print("Rejoining Vehicular")
+
+vehicular = pd.read_csv("interbank-internacional-2019/ib_base_vehicular/ib_base_vehicular.csv")
+vehicular1 = vehicular.groupby(["id_persona", "marca"]).veh_var1.sum().unstack(level=1, fill_value=0).astype("float32")
+vehicular2 = vehicular.groupby(["id_persona", "marca"]).veh_var2.sum().unstack(level=1, fill_value=0).astype("float32")
+del vehicular
+
+vehicular1.columns = [c + "_v1" for c in vehicular1.columns]
+vehicular2.columns = [c + "_v2" for c in vehicular2.columns]
+
+X_train = X_train.set_index("prediction_id").astype("int32").reset_index().set_index("id_persona").join(vehicular1).join(vehicular2).join(reniec).join(sunat)
+X_test = X_test.set_index("prediction_id").astype("int32").reset_index().set_index("id_persona").join(vehicular1).join(vehicular2).join(reniec).join(sunat)
+
 ##############
 # Train DATA #
 ##############
